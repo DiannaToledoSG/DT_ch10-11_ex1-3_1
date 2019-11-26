c48a75e42d40d74483164623b4848e0df44735e0
Dianna A Toledo <56136290+DiannaToledoSG@users.noreply.github.com>
2019-11-26 12:12:33 -0800

    Add files via upload

---
+++ week11_exercises/week11_exercises/src/week11_exercises/Ch11_Ex1.java
@@ -0,0 +1,23 @@
+//HEADER 
+//Program Name: Ch11_Ex1
+//Author: Dianna A.H.H. Toledo
+//Class: CS160 Fall 2019
+//Date: 11/26/2019
+// Description: Review the documentation of java.awt.Rectangle.
+//Which methods are pure? Which are modifiers?
+package week11_exercises;
+
+public class Ch11_Ex1 {
+
+	public static void main(String[] args) {
+
+//     Rectangle box = new Rectangle(0, 0, 100, 200);
+		
+//  java.awt.Rectangle is a modifier method, since it is adjustable. 
+	}
+
+}
+
+/* FOOTER
+
+*/
